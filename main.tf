afe842f0d3af38e6348a0cfe5851e101b6cfb73a
aniketbadadhe49-devops <aniketbadadhe49@gmail.com>
2025-11-04 22:52:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 }
 
 #Subnet
-resource "aws_subnet" "main {
+resource "aws_subnet" "main" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.subnet_cidr
   map_public_ip_on_launch = true
