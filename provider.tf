2fbbefcb463f096bf096116c4dc63ac6f01dd604
aniketbadadhe49-devops <aniketbadadhe49@gmail.com>
2025-11-04 23:07:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,17 @@
 terraform {
- required_version = ">= 1.6.0"
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-     version = "~> 5.0"
-   }
- }
-    backend "local" {
-      path = "terraform.tfstate"
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
 }
+   # 👇 Local backend (stores terraform.tfstate locally)
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
