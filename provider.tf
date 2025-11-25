67125b294a6a6130d3208f72fb6028ea4ce89d42
aniketbadadhe49-devops <aniketbadadhe49@gmail.com>
2025-11-25 22:59:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,6 +12,14 @@ terraform {
   }
 }
 
+  backend "s3" {
+    bucket         = "aniket-s3-tfstate-bucket-name"  # Replace with your S3 bucket name
+    key            = "terraform.tfstate" # Path within the bucket
+    region         = "us-east-1"                   # AWS region where the bucket resides
+    encrypt        = true                          # Optional: Encrypt the state file at rest
+    dynamodb_table = "your-dynamodb-lock-table"    # Optional: For state locking (recommended)
+  }
+}
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
